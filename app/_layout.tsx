6208245936ef622ef78672858f441b0eea452c26
Lauro Weingartner Neto <laurowrn@gmail.com>
2025-04-17 17:33:22 -0300

    Adding Open Graph meta tags

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,6 +17,7 @@ import {
 import { initMercadoPago } from "@mercadopago/sdk-react";
 import { fontSize, horizontalScale } from "@/helpers/responsiveScaling";
 import getLanguageTag from "@/helpers/getLanguageTag";
+import { Helmet, HelmetProvider } from "react-native-helmet-async";
 
 export const unstable_settings = {
   initialRouteName: "index",
@@ -48,96 +49,115 @@ export default function RootLayout() {
   }
 
   return (
-    <SessionProvider>
-      <GatewayProvider baseUrl={BACKEND_BASE_URL}>
-        <PaperProvider theme={paperTheme}>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-              name="index"
-              options={{
-                headerShown: true,
-                header: ({ navigation, options }) => (
-                  <Appbar.Header
-                    style={{ paddingHorizontal: horizontalScale(20) }}
-                  >
-                    <TikkoIcons
-                      name="mark1"
-                      size={fontSize(60)}
-                      color={paperTheme.colors.primary}
-                    />
-                    <Appbar.Content title="" />
-                    <Button
-                      mode="contained"
-                      onPress={() => {
-                        router.navigate("/login");
-                      }}
+    <HelmetProvider>
+      <Helmet>
+        <meta
+          property="og:image:secure_url"
+          content="https://i.postimg.cc/K86YbZ64/3000x3000-Colmeia-Maram-Rosa-Save-The-Date.png"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="800" />
+        <meta property="og:title" content="Colmeia" />
+        <meta property="og:description" content="Bem-vindo à Colmeia" />
+        <meta
+          property="og:url"
+          content="https://www.tikko.com.br/next-events/1/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Tikko" />
+      </Helmet>
+      <SessionProvider>
+        <GatewayProvider baseUrl={BACKEND_BASE_URL}>
+          <PaperProvider theme={paperTheme}>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen
+                name="index"
+                options={{
+                  headerShown: true,
+                  header: ({ navigation, options }) => (
+                    <Appbar.Header
+                      style={{ paddingHorizontal: horizontalScale(20) }}
                     >
-                      Login
-                    </Button>
-                  </Appbar.Header>
-                ),
-              }}
-            />
-            <Stack.Screen name="colmeia-reflections" />
-            <Stack.Screen
-              name="error"
-              options={{
-                presentation: "transparentModal",
-                animation: "fade",
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name="success"
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name="cancel"
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name="politica-privacidade"
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name="termos-e-condicoes"
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name="sobre"
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name="login"
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name="(main)"
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name="next-events"
-              options={{
-                headerShown: false,
-              }}
-            />
-          </Stack>
-        </PaperProvider>
-      </GatewayProvider>
-    </SessionProvider>
+                      <TikkoIcons
+                        name="mark1"
+                        size={fontSize(60)}
+                        color={paperTheme.colors.primary}
+                      />
+                      <Appbar.Content title="" />
+                      <Button
+                        mode="contained"
+                        onPress={() => {
+                          router.navigate("/login");
+                        }}
+                      >
+                        Login
+                      </Button>
+                    </Appbar.Header>
+                  ),
+                }}
+              />
+              <Stack.Screen name="colmeia-reflections" />
+              <Stack.Screen
+                name="error"
+                options={{
+                  presentation: "transparentModal",
+                  animation: "fade",
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="success"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="cancel"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="politica-privacidade"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="termos-e-condicoes"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="sobre"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="login"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="(main)"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="next-events"
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack>
+          </PaperProvider>
+        </GatewayProvider>
+      </SessionProvider>
+    </HelmetProvider>
   );
 }
