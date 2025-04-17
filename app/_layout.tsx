7789c7cfd17a4e0143cab61993d7fbdfc41a2604
Lauro Weingartner Neto <laurowrn@gmail.com>
2025-04-17 18:10:00 -0300

    Fixing Open Graph image tag

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -51,25 +51,24 @@ export default function RootLayout() {
   return (
     <HelmetProvider>
       <Helmet>
+        <meta name="description" content="" />
+        <meta property="og:url" content="https://www.tikko.com.br/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Colmeia" />
+        <meta property="og:description" content="" />
         <meta
           property="og:image"
-          content="https://i.postimg.cc/K86YbZ64/3000x3000-Colmeia-Maram-Rosa-Save-The-Date.png"
+          content="https://opengraph.b-cdn.net/production/images/52928b8c-e7c8-4970-a088-f4795fb7b667.png?token=K0zAmRySFekNHyJBz4VWgYpYlTCS401VL-ZTU6er7XQ&height=1200&width=1200&expires=33280923923"
         />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="tikko.com.br" />
+        <meta property="twitter:url" content="https://www.tikko.com.br/" />
+        <meta name="twitter:title" content="Colmeia" />
+        <meta name="twitter:description" content="" />
         <meta
-          property="og:image:secure_url"
-          content="https://i.postimg.cc/K86YbZ64/3000x3000-Colmeia-Maram-Rosa-Save-The-Date.png"
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/images/52928b8c-e7c8-4970-a088-f4795fb7b667.png?token=K0zAmRySFekNHyJBz4VWgYpYlTCS401VL-ZTU6er7XQ&height=1200&width=1200&expires=33280923923"
         />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="800" />
-        <meta property="og:image:height" content="800" />
-        <meta property="og:title" content="Colmeia" />
-        <meta property="og:description" content="Bem-vindo à Colmeia" />
-        <meta
-          property="og:url"
-          content="https://www.tikko.com.br/next-events/1/"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Tikko" />
       </Helmet>
       <SessionProvider>
         <GatewayProvider baseUrl={BACKEND_BASE_URL}>
